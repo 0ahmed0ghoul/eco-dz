f0e878a9fa140702a823187d57efa8106c6aae23
Mohamed-Islem-2005 <mohamed2005islem@gmail.com>
2025-12-25 13:56:51 +0100

    Update destinations page with navbar, footer, and monument data improvements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,25 @@ import Signin from "./sections/Signin.jsx";
 import MonumentDetails from "./sections/MonumentDetails.jsx";
 import Contact from "./pages/Contact.jsx";
 import Travels from "./pages/Travels.jsx";
+import Destinations from "./pages/Destinations.jsx";
+import Footer from "./components/Footer.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/travels" element={<Travels />} />
-
-      <Route path="/monument/:id" element={<MonumentDetails />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/travels" element={<Travels />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/monument/:id" element={<MonumentDetails />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
