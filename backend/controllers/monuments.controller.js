bd4d79e3231c0b8ec86ef545dbe4dbed8cd29ff9
Mohamed-Islem-2005 <mohamed2005islem@gmail.com>
2025-12-31 07:48:01 +0100

    Merge main: resolve conflicts by using main branch code

--- backend/controllers/monuments.controller.js
+++
@@ -1,6 +0,0 @@
-import pool from "../db.js";
-
-export const getAllMonuments = async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM monuments");
-  res.json(rows);
-};
--- backend/routes/monuments.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllMonuments } from "../controllers/monuments.controller.js";
-
-const router = Router();
-
-router.get("/", getAllMonuments);
-
-export default router;
